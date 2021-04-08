33a5001852b9999b5e33a7c3305e4275f4f8f112
Sebastian Konieczek <23232301+sebastiankonieczek@users.noreply.github.com>
2021-04-08 19:16:00 +0200

    fix failing xml-deserialization

--- src/main/java/spring_course/my_bank_transactions/dto/TransactionDto.java
+++ src/main/java/spring_course/my_bank_transactions/dto/TransactionDto.java
@@ -2,65 +2,57 @@ package spring_course.my_bank_transactions.dto;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.math.BigDecimal;
 
-@JacksonXmlRootElement( localName = "transaction" )
-public class TransactionDto {
+@XmlRootElement( name = "transaction" )
+public class TransactionDto
+{
    @NotBlank
    @NotNull
-   @JacksonXmlProperty( isAttribute = true, localName = "reference" )
-   private String reference;
-   @Min( 100 ) @Max( 500 )
-   @JacksonXmlProperty( isAttribute = true, localName = "amount" )
-   private BigDecimal amount;
+   private String reference_;
+   @Min( 100 )
+   @Max( 500 )
+   private BigDecimal amount_;
    @NotBlank
    @NotNull
-   @JacksonXmlProperty( isAttribute = true, localName = "userId" )
-   private String receivingUserId;
-
-   public TransactionDto() {}
+   private String receivingUserId_;
 
    @JsonCreator
-   public TransactionDto( @JsonProperty( "reference" ) final String reference,
-                          @JsonProperty( "amount" ) final BigDecimal amount,
-                          @JsonProperty( "userId" ) final String receivingUserId ) {
-      this.reference = reference;
-      this.amount = amount;
-      this.receivingUserId = receivingUserId;
+   public TransactionDto(
+      @JsonProperty( "reference" ) final String reference,
+      @JsonProperty( "amount" ) final BigDecimal amount,
+      @JsonProperty( "userId" ) final String receivingUserId )
+   {
+      this.reference_ = reference;
+      this.amount_ = amount;
+      this.receivingUserId_ = receivingUserId;
    }
 
-   public String getReference() {
-      return reference;
+   public TransactionDto()
+   {
    }
 
-   public BigDecimal getAmount() {
-      return amount;
+   @JsonProperty( "reference" )
+   public String getReference()
+   {
+      return reference_;
    }
 
+   @JsonProperty( "amount" )
+   public BigDecimal getAmount()
+   {
+      return amount_;
+   }
+
+   @JsonProperty( "userId" )
    public String getReceivingUserId()
    {
-      return receivingUserId;
-   }
-
-   public void setReference( String reference )
-   {
-      this.reference = reference;
-   }
-
-   public void setAmount( BigDecimal amount )
-   {
-      this.amount = amount;
-   }
-
-   public void setReceivingUserId( String receivingUserId )
-   {
-      this.receivingUserId = receivingUserId;
+      return receivingUserId_;
    }
 }
